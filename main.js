9c0006956af3533742636fbc618d683da15dcf12
Amber Li (niconotpig) <niconotpig@gmail.com>
2025-09-01 19:30:47 -0400

    day 1 #2

--- main.js
+++ main.js
@@ -3,27 +3,57 @@ import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 kaplay({
     width:600,
     height: 600,
-    background: [255, 230, 234]
+    background: [255, 230, 234],
 });
 
-const container = add([
-    rect(500,500),
-    pos(50,50),
-    color(240, 189, 198),
-]);
+scene("menu", () => {
+    const container = add([
+        rect(500,500),
+        anchor("center"),
+        pos(center()),
+        color(240, 189, 198),
+    ]);
 
-const stbtn = container.add([
-    rect(200,20),
-    pos(150,240),
-]);
+    const stbtn = add([
+        rect(200,30),
+        anchor("center"),
+        pos(center()),
+        area(),
+    ]);
 
-stbtn.add([
-    pos(85.9,5),
-    color(133, 57, 70),
-    text("start", {
-        size: 10,
-        letterSpacing:0,
-        width:28.2,
-    }),
+    stbtn.onHover(() => {
+        stbtn.scale = vec2(1.1,1.1);
+    });
+    stbtn.onHoverEnd(() => {
+        stbtn.scale = vec2(1,1);
+    });
+    stbtn.onClick (() => {
+        go("game");
+    });
 
-])
+    stbtn.add([
+        anchor("center"),
+        color(133, 57, 70),
+        text("start", {
+            size: 20,
+            letterSpacing:0,
+        }),
+    ]);
+
+})
+
+scene("game", () => {
+    const container = add([
+        rect(500,500),
+        anchor("center"),
+        pos(center()),
+        color(240, 189, 198),
+    ]);
+})
+
+go("menu");
+
+
+
+
+
