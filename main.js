496fcd0ca976b4bd18d6f5fe8e021c07fdba300e
Amber Li (niconotpig) <niconotpig@gmail.com>
2025-10-30 21:09:20 -0400

    working win lose system

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const board = document.getElementById("board")
 const score = document.getElementById("score")
+const result = document.getElementById("result")
 const grid = 4
 let cats = []
 let scoreValue = 0
@@ -18,22 +19,45 @@ function gameBoard() {
 }
 gameBoard()
 
+//are there zeros on the board
 function checkEmptySpace() {
+    let zeros = 0
     for (let i=0; i<16; i++) {
         if (cats[i].innerHTML == 0) {
-            return true
-        } else return false
-    } 
+            zeros ++
+        }
+    }
+    if (zeros === 0) {
+        return false 
+    } return true
 }
+//false = no lose
+function checkHorizontal() {
+    for (let i=0; i<15; i++) {
+        if (cats[i].innerHTML === cats[i+1].innerHTML) {
+            return false
+        } else return true 
+    }
+}
+checkHorizontal()
+//false = no lose
+function checkVertical() {
+    for (let i=0; i<4; i++) {
+        if (cats[i].innerHTML === cats[i+4].innerHTML) {
+            return false
+        } else return true
+    }
+}
+checkVertical()
 function generate() {
     let emptyspace = checkEmptySpace()
     if (emptyspace == true) {
     const randomNumber = Math.floor(Math.random() * cats.length)
-    if (cats[randomNumber].innerHTML == 0){
-        cats[randomNumber].innerHTML = 2      
-    } else {
-        generate()
-    }
+        if (cats[randomNumber].innerHTML == 0){
+            cats[randomNumber].innerHTML = 2      
+        } else {
+            generate()
+        }
     }
 }
 
@@ -75,8 +99,7 @@ function left() {
             cats[i+1].innerHTML = newRow[1]
             cats[i+2].innerHTML = newRow[2]
             cats[i+3].innerHTML = newRow[3]
-        }
-            
+        }     
     }
 }
 
@@ -90,7 +113,6 @@ function up() {
         let missing = 4 - filter.length
         let zeros = Array(missing).fill(0)
         let newColumn = filter.concat(zeros)
-        console.log(newColumn)
         cats[i].innerHTML = newColumn[0]
         cats[i+4].innerHTML = newColumn[1]
         cats[i+8].innerHTML = newColumn[2]
@@ -107,7 +129,6 @@ function down() {
         let missing = 4 - filter.length
         let zeros = Array(missing).fill(0)
         let newColumn = zeros.concat(filter)
-        console.log(newColumn)
         cats[i].innerHTML = newColumn[0]
         cats[i+4].innerHTML = newColumn[1]
         cats[i+8].innerHTML = newColumn[2]
@@ -125,6 +146,7 @@ function combineRow() {
         }
     }
     win()
+    lose()
 }
 function combineColumn() {
     for (let i = 0; i<12; i++) {
@@ -136,48 +158,48 @@ function combineColumn() {
             score.innerHTML = scoreValue
         }
     }
-    win()
+
 }
 function win() {
+    let win = winVar()
+    if(win == true)
+        result.style.display = 'block'
+        result.innerHTML = 'you win!! keep playing iyw'
+}
+function winVar() {
     for (let i=0; i<16; i++) {
-        if (cats[i].innerHTML == 2048)
-        document.getElementById("result").innerHTML = 'You win!! but you can keep playing'
+        if (cats[i].innerHTML == 2048){
+            return true 
+        } return false
     }
 }
-function gameOver() {
+
+function lose() {
+    let lose = loseVar()
     if (lose == true) {
+        result.style.display = 'block'
         document.getElementById("result").innerHTML = 'wuh woh you lost'
     }
 }
-function lose() {
-    let zeros = 0
-    for (let i=0;i<16; i++) {
-        if (cats[i].innerHTML == 0) {
-            zeros ++
-        }
-    }
-    if (zeros === 0) {
-        return checkHorizontal && checkVertical
 
+function loseVar() {
+    let emptyspace = checkEmptySpace()
+    let horizontal = checkHorizontal()
+    let vertical = checkVertical()
+    if (emptyspace == false) {
+        return horizontal && vertical
     } else return false 
 }
 
-function checkHorizontal() {
-    for (let i=0; i<15; i++) {
-        if (cats[i].innerHTML === cats[i+1].innerHTML) {
-            return false
-        } else return true
-    }
-}
-function checkVertical() {
-    for (let i=0; i<4; i++) {
-        if (cats[i].innerHTML === cats[i+4].innerHTML) {
-            return false
-        } else return true
-    }
-}
+
 
 function detectKey(k) {
+    checkEmptySpace()
+    checkHorizontal()
+    checkVertical()
+    winVar()
+    loseVar()
+    console.log (loseVar(), winVar(), checkEmptySpace())
     if (k.key === 'ArrowLeft'){
         keyLeft()
     } else if (k.key === 'ArrowRight') {
